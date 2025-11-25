04ae14177366e5a43bbea1dc6e76fe8b7fef1a2a
OlofKers <kerssemeeckers.olaf@gmail.com>
2025-11-25 17:57:24 +0100

    now rendering all 3 posts through javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const postFeed = document.getElementById("post-feed")
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -27,4 +28,41 @@ const posts = [
         likes: 152
     }
 ]
+
+let postFeedCode = ""
+
+function renderPosts(){
+    for (let i = 0; i < posts.length; i++){
+        postFeedCode += 
+        `<section class="post-creator">
+            <img class="creator-img" src=${posts[i].avatar}>
+            <p><span class="bold-text">${posts[i].name}</span><span class="location">${posts[i].location}</span></p>
+        </section>
+        <section class="post-content">
+            <img class="post-img" src=${posts[i].post}>
+        </section>
+        <section class="post-bottom">
+            <div class="post-controls">
+                <figure>
+                    <img class="like-btn" src="images/icon-heart.png">
+                    <figcaption class="caption">${posts[i].likes}</figcaption>
+                </figure>
+                <figure>
+                    <img class="comment-btn" src="images/icon-comment.png">
+                    <figcaption class="caption">0</figcaption>
+                </figure>
+                <figure>
+                    <img class="share-btn" src="images/icon-dm.png">
+                    <figcaption class="caption">0</figcaption>
+                </figure>
+            </div>
+            <p><span class="bold-text">${posts[i].username}</span>${posts[i].comment}</p>    
+        </section>`
+    }
+    return postFeedCode
+}
+
+
+postFeed.innerHTML += renderPosts()
+
 
